6e675a0ae6e938925358ccdb291c2af29f50591d
Joseph Cagle <joseph@josephcagle.com>
2020-03-05 15:10:21 -0500

    Update synctohomedir.sh

--- synctohomedir.sh
+++ synctohomedir.sh
@@ -11,12 +11,13 @@ do
 done
 
 echo "Making script for updating dotfiles in $HOME ..."
-cat <<END > $HOME/.updatedotfiles.sh && chmod u+x $HOME/.updatedotfiles.sh
+cat <<END > $HOME/.updatedotfiles.sh && \
+  chmod u+x $HOME/.updatedotfiles.sh && \
+   echo "Created .updatedotfiles.sh"
 #!/bin/bash
 echo "cd'ing to $HOME/Programming/mydotfiles."
 (cd $HOME/Programming/mydotfiles && exec ./synctohomedir.sh)
 END
-echo "Created .updatedotfiles.sh"
 
 echo "All done!"
 
