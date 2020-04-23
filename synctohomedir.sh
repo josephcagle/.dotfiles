db6e7f5731af8f052b3356a5df5f295c2f63169f
Joseph Cagle <joseph@josephcagle.com>
2020-04-23 19:02:29 -0400

    Revamp sync script

--- synctohomedir.sh
+++ synctohomedir.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-echo 'Be sure to `git` the latest version if yours is behind.' 
+echo 'Be sure to `git pull --recurse-submodules` the latest version if your version is behind.'
 echo "Copying files..."
 
-for dotfile in .*
-do
-    [ $dotfile != . ] &&                                                        \
-    [ $dotfile != .. ] &&                                                       \
-    [ $dotfile != .DS_Store ] &&                                                \
-    [ $dotfile != .git ] &&                                                     \
-    [ $dotfile != .gitignore ] &&                                               \
-    [ $dotfile != .mailmap ] &&                                                 \
-    [ ! "$(echo $dotfile | grep -qE '.swp$' ; echo $?)" = 0 ] &&                \
-#    for otherfile in $(cat .gitignore); do [ $dotfile != $otherfile ]; done &&  \
-    cp -riv $dotfile $HOME
+overwrite_all=0
+for dotfile in $(cat dotfiles.list | grep -v '^#') ; do
+    if git check-ignore $dotfile ; then
+        continue
+    fi
+
+    if [ -e ~/$dotfile -a $overwrite_all = 0 ]; then
+        echo -n 'Overwrite ~/'$dotfile'? (y/n/a) '
+	read -rn 1 response ; echo
+	case $response in
+        *a*)
+            overwrite_all=1
+	    ;;
+	*y*)
+            ;;
+	*)
+	    continue
+	    ;;
+	esac
+    fi
+    rm -rf $HOME/$dotfile
+    cp -rv $dotfile $HOME/$dotfile
 done
+
+
+# (Old script)
+
+#for dotfile in .*
+#do
+#    [ $dotfile != . ] &&                                                        \
+#    [ $dotfile != .. ] &&                                                       \
+#    [ $dotfile != .DS_Store ] &&                                                \
+#    [ $dotfile != .git ] &&                                                     \
+#    [ $dotfile != .gitignore ] &&                                               \
+#    [ $dotfile != .mailmap ] &&                                                 \
+#    [ ! "$(echo $dotfile | grep -qE '.swp$' ; echo $?)" = 0 ] &&                \
+##    for otherfile in $(cat .gitignore); do [ $dotfile != $otherfile ]; done &&  \
+#    cp -riv $dotfile $HOME
+#done
 
 #echo "Making script for updating dotfiles in $HOME ..."
 #cat <<END > $HOME/.updatedotfiles.sh && \
