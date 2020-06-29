593fb1306b8a51028f87148f27afb26ef62d84ca
Joseph Cagle <joseph@josephcagle.com>
2020-06-29 19:56:49 -0400

    Update install.sh to use /tmp

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [ -e mydotfiles ] ; then
-    echo $0": Err: mydotfiles already exists. Exiting."
+if [ -e /tmp/mydotfiles ] ; then
+    echo $0": Err: /tmp/mydotfiles already exists. Exiting."
     exit 1
 fi
 set -e
 echo "WARNING: This will overwrite existing config in your home directory."
 echo "Press enter to continue."
 read
-git clone https://github.com/DarthFloopy/mydotfiles.git --recurse-submodules
-cd mydotfiles
+git clone --recurse-submodules https://github.com/DarthFloopy/mydotfiles.git /tmp/mydotfiles
+cd /tmp/mydotfiles
 echo a | ./synctohomedir.sh
 cd - > /dev/null
-rm -rf mydotfiles
+rm -rf /tmp/mydotfiles
 exit 0
 
