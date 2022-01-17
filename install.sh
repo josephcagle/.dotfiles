b55a797159eee8f42c1a598833b4cf71196da0fd
Joseph Cagle <joseph@josephcagle.com>
2022-01-17 17:25:33 -0500

    Remove old code from install.sh

--- install.sh
+++ install.sh
@@ -49,28 +49,3 @@ done
 
 echo "All done!"
 
-# (Old script)
-
-#for dotfile in .*
-#do
-#    [ $dotfile != . ] &&                                                        \
-#    [ $dotfile != .. ] &&                                                       \
-#    [ $dotfile != .DS_Store ] &&                                                \
-#    [ $dotfile != .git ] &&                                                     \
-#    [ $dotfile != .gitignore ] &&                                               \
-#    [ $dotfile != .mailmap ] &&                                                 \
-#    [ ! "$(echo $dotfile | grep -qE '.swp$' ; echo $?)" = 0 ] &&                \
-##    for otherfile in $(cat .gitignore); do [ $dotfile != $otherfile ]; done &&  \
-#    cp -riv $dotfile $HOME
-#done
-
-#echo "Making script for updating dotfiles in $HOME ..."
-#cat <<END > $HOME/.updatedotfiles.sh && \
-#  chmod u+x $HOME/.updatedotfiles.sh && \
-#   echo "Created .updatedotfiles.sh"
-##!/bin/bash
-#echo "cd'ing to $HOME/Programming/mydotfiles."
-#(cd $HOME/Programming/mydotfiles && exec ./synctohomedir.sh)
-#END
-
-
